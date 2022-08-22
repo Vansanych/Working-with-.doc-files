6bfac01dfb18756f571d5df824f867499aae5bc7
Ivan <111785086+Vansanych@users.noreply.github.com>
2022-08-22 21:34:12 +0300

    comments deleted

--- py-docx.py
+++ py-docx.py
@@ -12,30 +12,3 @@ for t in range(len(par[0].runs)):
 
 print(dir(docx))
 
-
-# print(docx.text(h))
-
-# print(par[0].runs[0].text.Font.name)
-
-# print(par[0].runs[0].text)
-# print(par[0].runs[1].text)
-# tab = doc.tables
-# print(tab)
-# print(dir(docx.Document))
-#
-# print(dir(docx.Document.__dict__))
-# print(dir(docx.Document()))
-# print(dir(docx.Document()) == dir(doc))
-# print(dir(doc.tables))
-#
-# print(dir(doc.tables[0]))
-# print(dir(doc.tables[0].rows[0]))
-# print(dir(doc.tables[0].rows[0].cells[0]))
-#
-# print(dir(doc.tables[0].rows[0].cells[0].text))
-# print('text', doc.tables[0].rows[1].cells[0].text)
-# print('text', doc.tables[0].rows[1].cells[1].text)
-# # doc.tables[0].rows[1].cells[1].text = 'hbh'
-# print('text', doc.tables[0].rows[1].cells[1].text)
-#
-# doc.save(r'C:\Users\Иван\Desktop\Иван\python\pythonProject\Word\11.docx')
